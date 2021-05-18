c1e8c1f82b9359415f5b5a54bceb20d85a76fd5c
Poornima J <poornijaidip@gmail.com>
2021-05-18 23:24:51 +0530

    Sound input + mp3 to wav + visualisation static

--- f.py
+++ f.py
@@ -1,10 +1,13 @@
 from os import path
 from pydub import AudioSegment
+
+# imports
+import matplotlib.pyplot as plt
+import numpy as np
 import pyaudio
 import wave
 import sys
 
-BUFFER_SIZE = 1024
 print("Enter the audio file in mp3 format:- ")
 src = input()
 # files                                                                         
@@ -18,24 +21,61 @@ sound.export(dst, format="wav")
 # Opening audio file as binary data
 obj = wave.open(dst, 'rb')
 
-# Instantiate PyAudio
-p = pyaudio.PyAudio()
-file_sw = obj.getsampwidth()
+# #Code for playing
+# # Instantiate PyAudio
+# p = pyaudio.PyAudio()
+# file_sw = obj.getsampwidth()
 
-stream = p.open(format=p.get_format_from_width(file_sw),
-                channels=obj.getnchannels(),
-                rate=obj.getframerate(),
-                output=True)
+# stream = p.open(format=p.get_format_from_width(file_sw),
+#                 channels=obj.getnchannels(),
+#                 rate=obj.getframerate(),
+#                 output=True)
 
-data = obj.readframes(BUFFER_SIZE)
+# data = obj.readframes(-1)
 
-while len(data)>0:
-    stream.write(data)
-    data = obj.readframes(BUFFER_SIZE)
+# while len(data)>0:
+#     stream.write(data)
+#     data = obj.readframes(-1)
 
-stream.stop_stream()
-stream.close()
+signal = obj.readframes(-1)
+signal = np.frombuffer(signal, dtype ="int16")
+      
+    # gets the frame rate
+f_rate = obj.getframerate()
+  
+    # to Plot the x-axis in seconds 
+    # you need get the frame rate 
+    # and divide by size of your signal
+    # to create a Time Vector 
+    # spaced linearly with the size 
+    # of the audio file
+time = np.linspace(
+    0, # start
+    len(signal) / f_rate,
+    num = len(signal)
+)
+  
+    # using matlplotlib to plot
+    # creates a new figure
+plt.figure(1)
+plt.axis("off")
+      
+    # title of the plot
+# plt.title("Sound Wave")
+      
+    # label of x-axis
+# plt.xlabel("Time")
+     
+    # actual ploting
+plt.plot(time, signal)
+      
+    # shows the plot 
+    # in new window
+plt.show()
 
-p.terminate()
+# stream.stop_stream()
+# stream.close()
+
+# p.terminate()
 obj.close()
 
